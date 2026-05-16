a8d17397f87a6efbc4dfe1852b6f1f0f8a2c9293
johnchizzy <johnchizzy85@gmail.com>
2026-05-16 03:18:10 +0100

    deletes statscard

--- src/app/models/developer.ts
+++
@@ -1,8 +0,0 @@
-export interface StatsCardProps {
-  title: string;
-  value: string | number;
-  className?: string;
-  isLoading?: boolean;
-  icon?: string;
-  iconComponent?: React.ReactNode;
-}
